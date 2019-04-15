b27e45b2a4bdff319936c500e2d86ab0863dffe2
DizzyJump <mojpovelitel@gmail.com>
2019-04-15 22:30:05 +0300

    initial commit

---
+++ MyBalls/Assets/Scripts/Common/Events/EventObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Events/Base Event")]
+public class EventObject : ScriptableObject {
+    public delegate void CustomEvent();
+    event CustomEvent OnInvoke = ()=> { };
+    public string Description;
+
+    public void Invoke()
+    {
+        OnInvoke.Invoke();
+    }
+
+    public void AddHandler(CustomEvent func)
+    {
+        OnInvoke += func;
+    }
+
+    public void RemoveHandler(CustomEvent func)
+    {
+        OnInvoke -= func;
+    }
+}
